9fea947cf433ed39b0240cb15906fcaff9933398
abbasalirezaei <abbasalirezaei.79@gmail.com>
2022-08-03 15:48:05 +0430

    end of create varaable

--- 1.js
+++ 1.js
@@ -13,3 +13,15 @@ console.log(access)
 // if you wanna change const give you error
 // access='no';
 // console.log(access)
+
+
+// what is the diffrese betwwen let and var
+// 1= you can create two var with same name
+// 2- you can use it in scope 
+// 
+var f1='nothing for test';
+let f2='abbasali';
+
+var f1='raee';
+// give you error 
+// let f2='3333'
